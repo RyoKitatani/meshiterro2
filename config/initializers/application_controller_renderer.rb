2cf556cd806c67230d01a345fd3be20f6e1504ee
RyoKitatani <ryo0929m@gmail.com>
2021-02-21 16:47:13 +0000

    画像投稿機能追加_new+show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b3ca50a74932d553599de5d9643813e334fcbc5382f099956b485200ade2daea5d998b4c5255143a2ca2fc52f6aeff3c75a50358246a058c205e9599dbfc7e20'
